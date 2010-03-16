78da0e701d730f3f852ab7eb6105a4ec9def6684
James Golick <james@giraffesoft.ca>
2010-03-16 16:06:58 -0700

    add thrift and fix build

--- project/build/TelephosProject.scala
+++ project/build/TelephosProject.scala
@@ -6,9 +6,10 @@ class TelephosProject(info: ProjectInfo) extends DefaultProject(info) {
   val mongodb      = "com.mongodb" % "mongodb" % "1.0" from "http://cloud.github.com/downloads/mongodb/mongo-java-driver/mongo-1.0.jar"
   val mockito      = "org.mockito" % "mockito" % "1.8.0" from "http://mockito.googlecode.com/files/mockito-all-1.8.0.jar"
 
-  def javaDirectoryPath   = "src_managed" / "main" / "java"
-  def thriftDirectoryPath = "src_managed" / "main" / "thrift"
-  def thriftFile          = thriftDirectoryPath / "cassandra.thrift"
+  def javaDirectoryPath           = "src_managed" / "main" / "java"
+  override def mainJavaSourcePath = javaDirectoryPath
+  def thriftDirectoryPath         = "src_managed" / "main" / "thrift"
+  def thriftFile                  = thriftDirectoryPath / "cassandra.thrift"
 
   def thriftTask(tpe: String, directory: Path, thriftFile: Path) = {
     val cleanIt = cleanTask(directory) named("clean-thrift-" + tpe)
