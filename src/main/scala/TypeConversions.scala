0facaa58e09e7b6fd3bb31a26ea55d043fcce45c
James Golick <james@giraffesoft.ca>
2010-03-18 18:11:13 -0700

    trailing space

--- src/main/scala/TypeConversions.scala
+++ src/main/scala/TypeConversions.scala
@@ -4,7 +4,7 @@ object TypeConversions {
   type ColumnMap = Map[Array[Byte], Array[Byte]]
 
   implicit def string2ByteArray(string: String): Array[Byte] = string.getBytes
-  implicit def map2ColumnMap[A <% Array[Byte], B <% Array[Byte]](map: Map[A, B]): 
+  implicit def map2ColumnMap[A <% Array[Byte], B <% Array[Byte]](map: Map[A, B]):
     ColumnMap = {
     map.foldLeft(Map[Array[Byte], Array[Byte]]()) { case (map, (k, v)) =>
       val tuple = (k: Array[Byte], v: Array[Byte])
