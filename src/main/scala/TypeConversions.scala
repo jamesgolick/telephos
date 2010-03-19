503b83888eace7d47031c5b26960c83d19702bf0
James Golick <james@giraffesoft.ca>
2010-03-19 08:46:30 -0700

    add a couple more convenience conversions

--- src/main/scala/TypeConversions.scala
+++ src/main/scala/TypeConversions.scala
@@ -12,5 +12,14 @@ object TypeConversions {
     }
   }
 
+  implicit def columnMap2Map[A, B](map: Map[Array[Byte], Array[Byte]])
+    (implicit f: Array[Byte] => A, g: Array[Byte] => B): Map[A, B] = {
+    map.foldLeft(Map[A, B]()) { case(map, (k, v)) => 
+      val tuple = (k: A, v: B)
+      map + tuple
+    }
+  }
+
   implicit def uuid2ByteArray(uuid: UUID): Array[Byte] = uuid.asByteArray
+  implicit def byteArray2UUID(bytes: Array[Byte]): UUID = UUID.valueOf(bytes)
 }
