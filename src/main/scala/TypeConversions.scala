197ccc980da83470e83b514f8561c386dbbe5d95
James Golick <james@giraffesoft.ca>
2010-03-18 18:09:17 -0700

    bit of a cleanup thanks to the #scala guys

--- src/main/scala/TypeConversions.scala
+++ src/main/scala/TypeConversions.scala
@@ -4,9 +4,11 @@ object TypeConversions {
   type ColumnMap = Map[Array[Byte], Array[Byte]]
 
   implicit def string2ByteArray(string: String): Array[Byte] = string.getBytes
-  implicit def map2ColumnMap[A <% Array[Byte], B <% Array[Byte]](map: Map[A, B]): ColumnMap = {
-    map.foldLeft(Map[Array[Byte], Array[Byte]]()) { (map, kv) =>
-      map + Tuple2[Array[Byte], Array[Byte]](kv._1, kv._2)
+  implicit def map2ColumnMap[A <% Array[Byte], B <% Array[Byte]](map: Map[A, B]): 
+    ColumnMap = {
+    map.foldLeft(Map[Array[Byte], Array[Byte]]()) { case (map, (k, v)) =>
+      val tuple = (k: Array[Byte], v: Array[Byte])
+      map + tuple
     }
   }
 }
