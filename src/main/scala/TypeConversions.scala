306d8847e46497d3c0ec3168755ad6e4a3005431
James Golick <james@giraffesoft.ca>
2010-03-18 22:36:29 -0700

    add a type conversion from a JUG UUID to Array[Byte]

--- src/main/scala/TypeConversions.scala
+++ src/main/scala/TypeConversions.scala
@@ -1,14 +1,16 @@
 package com.protose.telephos
 
-object TypeConversions {
-  type ColumnMap = Map[Array[Byte], Array[Byte]]
+import org.safehaus.uuid.UUID
 
+object TypeConversions {
   implicit def string2ByteArray(string: String): Array[Byte] = string.getBytes
   implicit def map2ColumnMap[A <% Array[Byte], B <% Array[Byte]](map: Map[A, B]):
-    ColumnMap = {
+    Map[Array[Byte], Array[Byte]] = {
     map.foldLeft(Map[Array[Byte], Array[Byte]]()) { case (map, (k, v)) =>
       val tuple = (k: Array[Byte], v: Array[Byte])
       map + tuple
     }
   }
+
+  implicit def uuid2ByteArray(uuid: UUID): Array[Byte] = uuid.asByteArray
 }
